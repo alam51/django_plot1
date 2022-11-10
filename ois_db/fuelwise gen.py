92582747c624f676eef4eedbb21817a57dbbf070
alam51 <jccbuet1@gmail.com>
2022-11-10 17:23:13 +0600

    fuelwise gen.py added

--- ois_db/fuelwise gen.py
+++ ois_db/fuelwise gen.py
@@ -1,7 +1,7 @@
 from utils import CONNECTOR_LDD
 import pandas as pd
 
-from_datetime = '2022-10-3 00:00'
+from_datetime = '2022-9-19 00:00'
 to_datetime = '2022-10-4 23:00'
 query_str = f"""
 SELECT mw.date_time, g.grid_name, ps.fuel_id, f.name AS fuel_name, SUM(mw.value) as gen_mw
@@ -20,13 +20,16 @@ df['date'] = df.index.date
 df['time'] = df.index.time
 a = 4
 
-date_range = pd.date_range(start=from_datetime, end=to_datetime, freq='1D')
-for date in date_range:
-    current_df = df.loc[date: date + pd.to_timedelta('1D')]
-    # east_df = current_df[current_df.grid_name == 'Eastern']
-    # east_df_processed = east_df.pivot(index=['date', 'grid_name', 'fuel_name'], columns=['time'], values=['gen_mw'])
-    # east_df_processed.to_excel('gen.xlsx')
+# date_range = pd.date_range(start=from_datetime, end=to_datetime, freq='1D')
+# for date in date_range:
+#     current_df = df.loc[date: date + pd.to_timedelta('1D')]
+#     # east_df = current_df[current_df.grid_name == 'Eastern']
+#     # east_df_processed = east_df.pivot(index=['date', 'grid_name', 'fuel_name'], columns=['time'], values=['gen_mw'])
+#     # east_df_processed.to_excel('gen.xlsx')
+#
+#     current_df_processed = current_df.pivot(index=['date', 'grid_name', 'fuel_name'], columns=['time'], values=['gen_mw'])
+#     current_df_processed.to_excel('gen.xlsx')
 
-    current_df_processed = current_df.pivot(index=['date', 'grid_name', 'fuel_name'], columns=['time'], values=['gen_mw'])
-    current_df_processed.to_excel('gen.xlsx')
+df_processed = df.pivot(index=['date', 'grid_name', 'fuel_name'], columns=['time'], values=['gen_mw'])
+df_processed.to_excel('gen.xlsx')
 v = 1
